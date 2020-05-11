9e156053f6dcfacdad1fe9fd712f814395cd9d67
Rachel Kim <rkim5@stevens.edu>
2020-05-10 21:01:19 -0400

    Modified App.js, added components folder, and added Main and UserPage

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../App.css';
+
+function Main() {
+	return (
+		<div>
+			<p>Stuck at home?</p>
+			<p>Browse our website for recipes people made during Quarantine 2020.</p>
+		</div>
+	);
+}
+
+export default Main;
