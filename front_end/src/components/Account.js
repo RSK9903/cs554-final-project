0f4006e281dcd8e315952ccc41e3be4bc4e129ca
Rachel Kim <rkim5@stevens.edu>
2020-05-13 22:21:25 -0400

    Modifed account page to display user info

--- front_end/src/components/Account.js
+++ front_end/src/components/Account.js
@@ -1,16 +1,36 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../firebase/Auth';
 import SignOutButton from './SignOut';
 import '../App.css';
 import ChangePassword from './ChangePassword';
 import ChangeInfo from './ChangeInfo';
+import API from '../API';
 
 function Account() {
 	const { currentUser } = useContext(AuthContext);
+	const [ user, setUser ] = useState(undefined);
+	useEffect(
+		() => {
+			async function fetchData() {
+				try {
+					let id = currentUser.uid;
+					const { data: userInfo } = await API.get("users/"+id);
+					setUser(userInfo);
+				} catch (e) {
+					console.log(e);
+				}
+			}
+			fetchData();
+		},
+		[]
+	);
 
 	return (
 		<div>
 			<h2>Account Page</h2>
+			<h3>Name: {user&&user.firstName} {user&&user.lastName}</h3>
+			<h3>Email: {user&&user.email}</h3>
+			<h3>Birthday: {user && user.birthday}</h3>
 			<ChangeInfo />
 			<ChangePassword />
 			<SignOutButton />
