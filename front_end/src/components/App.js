3c6f0c8c3de7d6d395bbad286e78ee6fa13281cd
Rachel Kim <rkim5@stevens.edu>
2020-05-16 00:32:05 -0400

    Edited user pages

--- front_end/src/components/App.js
+++ front_end/src/components/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 
-// import '../App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Account from "./Account";
 import Home from "./Home";
 import Navigation from "./Navigation";
--- front_end/src/components/SingleUser.js
+++ front_end/src/components/SingleUser.js
@@ -3,43 +3,74 @@ import { AuthContext } from "../firebase/Auth";
 import "../App.css";
 import API from "../API";
 import UserReviewList from "./UserReviewList";
+import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 const User = (props) => {
   const { currentUser } = useContext(AuthContext);
-  const [userData, setUser] = useState(undefined);
-  const [reviews, setReviews] = useState(undefined);
-  const [recipeData, setRecipe] = useState(undefined);
+  const [user, setUser] = useState(undefined);
+	const [recipes, setRecipes] = useState(undefined);
 
   useEffect(() => {
     async function fetchData() {
         try {
-            if(!props.match){
-                const { data:user } = await API.get("/users/"+currentUser.uid);
-                setUser(user);
-            } else {
-                const { data:user } = await API.get("/users/"+props.match.params.id);
-                setUser(user);
-            }
-      } catch (e) {
-        console.log(e);
-      }
+          const { data:user } = await API.get("/users/"+props.match.params.id);
+          const { data: recipeList } = await API.get("recipes/user/"+props.match.params.id);
+          setUser(user);
+          setRecipes(recipeList);
+        } catch (e) {
+          console.log(e);
+        }
     }
     fetchData();
   },
-  []);
+  [props.match.params.id]);
 
+  const createRecipeLine = (recipe) => {
+		return <li><Link to={`/recipes/${recipe._id}`}>{recipe.title}</Link></li>;
+	  };
+
+	const recipeli =
+    recipes &&
+    recipes.map((i) => {
+      return createRecipeLine(i);
+  });
+  
   return(
-    <div>
-    <div className="userPage">
-        <h3>{userData && userData.firstName} {userData && userData.lastName}</h3>
-		    <p>Birthday: {userData && userData.birthday}</p>
-        <h3>Recipes</h3>
-        <ul>
-            <li>Recipe Placeholder</li>
-        </ul>
-    </div>
-    <UserReviewList />
-    </div>
+    <Container>
+			<Row>
+				<Col>
+					<Tabs defaultActiveKey="myInfo" id="uncontrolled-tab-example" style={{ marginTop: '5%' }}>
+						<Tab eventKey="myInfo" title="User Info">
+							<div className="userPage">
+								<h3>{user && user.firstName} {user && user.lastName}</h3>
+								<h4>Birthday: {user && user.birthday}</h4>
+							</div>
+						</Tab>
+            <Tab eventKey="viewRecipes" title="Recipes">
+              <div className="userPage">
+                <h3>Recipes</h3>
+                <ul>{recipeli}</ul>
+              </div>
+						</Tab>
+						<Tab eventKey="viewReviews" title="Reviews">
+							<UserReviewList id={props.match.params.id}/>
+						</Tab>
+					</Tabs>
+				</Col>
+			</Row>
+		</Container>
+    // <div>
+    // <div className="userPage">
+    //     <h3>{userData && userData.firstName} {userData && userData.lastName}</h3>
+		//     <p>Birthday: {userData && userData.birthday}</p>
+    //     <h3>Recipes</h3>
+    //     <ul>
+    //         <li>Recipe Placeholder</li>
+    //     </ul>
+    // </div>
+    // <UserReviewList />
+    // </div>
   );
 }
 
