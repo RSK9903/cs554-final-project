e0f47a951d07b981d418a25097823ecfe085fe3c
Rachel Kim <rkim5@stevens.edu>
2020-05-16 03:08:56 -0400

    Changed recipe page so that if user already reviewed, cannot review again

--- front_end/src/components/RecipeReviewList.js
+++ front_end/src/components/RecipeReviewList.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
 import API from "../API";
+import {Link} from "react-router-dom";
 
 const RecipeReviewList = (props) => {
   const [reviewData, setReviewData] = useState(undefined);
+	const [setAuthor, setGetAuthor] = useState(false);
   let li = null;
 
   useEffect(() => {
@@ -16,22 +18,36 @@ const RecipeReviewList = (props) => {
       }
     }
     fetchData();
-  });
+  }, []);
+  
+  const getAuthor = async () => {
+    let index;
+    for(index in reviewData){
+      let review = reviewData[index];
+      const {data:user} = await API.get("users/"+review.author_id);
+      review.author_name = user.firstName + " " + user.lastName;
+    }
+    setGetAuthor(true);
+  };
 
-  li =
-    reviewData &&
-    reviewData.map((review) => {
-      return (
-        <li>
-          Rating: {review.rating} Comment: {review.comment}
-        </li>
-      );
-    });
+  if (reviewData && !setAuthor) {
+    getAuthor();
+  }
+
+  li = reviewData && setAuthor && reviewData.map((review) => {
+    return(
+      <div>
+      <h4><Link to={`/users/${review.author_id}`}>{review.author_name}</Link></h4>
+          <p>Rating: {review.rating}</p>
+          <p>Comment: {review.comment}</p>
+      </div>
+    )
+  });
 
   return (
     <div className="App-body">
       <h3 class="recipe-review-list-header">Reviews for this Recipe</h3>
-      <ul class="recipe-review-list">{li}</ul>
+      <div class="recipe-review-list">{li}</div>
     </div>
   );
 };
