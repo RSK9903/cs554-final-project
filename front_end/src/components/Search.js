725218dcdef7d662793100bc20e2baf78dbb9500
Diaeddin Motan <dmotan@macIT009028.local>
2020-05-17 02:13:01 -0400

    By dmotan: Fixed search

--- front_end/src/components/Search.js
+++ front_end/src/components/Search.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Container, Row, Col, Form } from 'react-bootstrap';
 
 const Search = (props) => {
   const handleChange = (e) => {
     props.searchValue(e.target.value);
   };
   return (
-    <form method="POST " name="formName">
-      <label>
-        Search Recipes:
-        <input type="text" name="searchTerm" onChange={handleChange} />
-      </label>
-    </form>
+    <Container>
+      <Row>
+        <Col>
+          <Form method="POST " name="formName" style={{ marginTop: '5%' }}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Search Recipes</Form.Label>
+              <Form.Control type="text" name="searchTerm" onChange={handleChange} />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
