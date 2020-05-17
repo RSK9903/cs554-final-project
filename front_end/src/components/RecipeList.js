39bf7033feade280cf61366f078eebe28849a75d
nbernhard314 <nbernhar@stevens.edu>
2020-05-17 00:04:30 -0400

    working recipe index sorting

--- front_end/src/components/RecipeList.js
+++ front_end/src/components/RecipeList.js
@@ -1,41 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Date } from "prismic-reactjs";
 import "../App.css";
 import API from "../API";
 
-const RecipeList = (props) => {
-  const [recipeData, setRecipeData] = useState(undefined);
-  let li = null;
+const RecipeList = () => {
+  const [recipeData, setRecipeData] = useState([]);
+  //const [byTitle, sortedByTitle] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const { data } = await API.get("/recipes");
+        data.reverse();
         setRecipeData(data);
       } catch (e) {
         console.log(e);
       }
     }
     fetchData();
-  });
+  }, []);
 
-  const buildListItem = (recipe) => {
-    return (
-      <li>
-        <Link to={`/recipes/${recipe._id}`}>{recipe.title}</Link>
-      </li>
-    );
-  };
-
-  li =
-    recipeData &&
-    recipeData.map((recipe) => {
-      return buildListItem(recipe);
-    });
-
-  const sort = (event) => {
-    let temp = recipeData;
-    if (event.target.value == "alpha") {
+  const sortByTitle = () => {
+    const temp = [...recipeData];
+    setRecipeData(
       temp.sort((a, b) => {
         const ra = a.title.toUpperCase();
         const rb = b.title.toUpperCase();
@@ -43,18 +31,23 @@ const RecipeList = (props) => {
         if (ra > rb) return 1;
         if (rb > ra) return -1;
         return 0;
-      });
-    } else if (event.target.value == "date") {
+      })
+    );
+    console.log(recipeData);
+  };
+
+  const sortByDate = () => {
+    const temp = [...recipeData];
+    setRecipeData(
       temp.sort((a, b) => {
         const da = a.datePosted;
         const db = b.datePosted;
 
-        if (da > db) return 1;
-        if (db > da) return -1;
+        if (da > db) return -1;
+        if (db > da) return 1;
         return 0;
-      });
-    }
-    setRecipeData(temp);
+      })
+    );
     console.log(recipeData);
   };
 
@@ -62,11 +55,27 @@ const RecipeList = (props) => {
     <div className="recipe-index">
       <h2 className="recipe-list-header">Recipe Index</h2>
       <label>Sort By: &nbsp;</label>
-      <select onChange={sort} defaultValue="date">
-        <option value="alpha">Title</option>
-        <option value="date">Date Posted</option>
-      </select>
-      <ul className="recipe-list">{li}</ul>
+      <button onClick={() => sortByTitle()}>Title</button>
+      <button onClick={() => sortByDate()}>Date Posted</button>
+      <table>
+        <tr>
+          <th>Recipe Title</th>
+          <th>Date Posted</th>
+        </tr>
+        {recipeData &&
+          recipeData.map((recipe) => (
+            <tr>
+              <th>
+                <Link to={`/recipes/${recipe._id}`}>{recipe.title}</Link>
+              </th>
+              <th>
+                {Date(recipe.datePosted).getMonth() + 1}/
+                {Date(recipe.datePosted).getDate()}/
+                {Date(recipe.datePosted).getFullYear()}
+              </th>
+            </tr>
+          ))}
+      </table>
     </div>
   );
 };
